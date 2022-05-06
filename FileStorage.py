b15613052ae4704a4d99aa1d9ecbcf2165b95a2c
KostryninaEkaterina <79746545+KostryninaEkaterina@users.noreply.github.com>
2022-05-06 23:05:00 +0300

    Add files via upload

--- FileStorage.py
+++ FileStorage.py
@@ -26,7 +26,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                         params_dict[k] = v
                 data_base = DataStorage()
                 json_obj = json.dumps(data_base.loading_by_params(params_dict), indent=4)
-                self.wfile.write(str(json_obj).encode('utf-8'))
+                self.wfile.write(json_obj.encode('utf-8'))
         elif self.path.startswith('/api/download'):
             params = parse_qs(urlparse(self.path).query)
             if 'id' in params:
@@ -35,21 +35,21 @@ class RequestHandler(BaseHTTPRequestHandler):
                 if database.get_name_by_id(id):
                     self.send_response(200)
                     self.end_headers()
-                    with open(database.get_name_by_id(id)[0][0], mode="rb") as body:
+                    with open(id, mode="rb") as body:
                         content = body.read()
                         self.wfile.write(content)
                 else:
                     self.send_response(404)
                     self.end_headers()
-                    self.wfile.write(str('файл не существует').encode('utf-8'))
+                    self.wfile.write('файл не существует'.encode('utf-8'))
             else:
                 self.send_response(400)
                 self.end_headers()
-                self.wfile.write(str('отсутствуют условия').encode('utf-8'))
+                self.wfile.write('отсутствуют условия'.encode('utf-8'))
         else:
             self.send_response(501)
             self.end_headers()
-            self.wfile.write(str('Not Implemented').encode('utf-8'))
+            self.wfile.write('Not Implemented'.encode('utf-8'))
 
     def do_POST(self):
         if self.path.startswith('/api/upload'):
@@ -79,17 +79,17 @@ class RequestHandler(BaseHTTPRequestHandler):
                     database.update(file_dict)
                 else:
                     database.save_in_table(file_dict)
-                with open(name, mode="wb") as file:
+                with open(id, mode="wb") as file:
                     file.write(body)
                 self.send_response(201)
                 self.send_header('Content-type', 'multipart/form-data')
                 self.end_headers()
                 json_obj = json.dumps([file_dict], indent=4)
-                self.wfile.write(str(json_obj).encode('utf-8'))
+                self.wfile.write(json_obj.encode('utf-8'))
         else:
             self.send_response(501)
             self.end_headers()
-            self.wfile.write(str('Not Implemented').encode('utf-8'))
+            self.wfile.write('Not Implemented'.encode('utf-8'))
 
     def do_DELETE(self):
         if self.path.startswith('/api/delete'):
